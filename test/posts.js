dfe42ef035b45b1e63761252afac06684e31ddbc
Rob Blake <rob.bl8ke@outlook.com>
2024-08-24 17:31:21 +0200

    Negative tests (look at the status code and response message)

--- test/posts.js
+++ test/posts.js
@@ -27,12 +27,46 @@ describe("User Posts", () => {
       
       expect(res.body).to.deep.include(data);
       postId = res.body.id;
-  });
+    });
     
-  it('GET /posts/:id', async () => {
-    await request
+    it('GET /posts/:id', async () => {
+      await request
       .get(`posts/${postId}`)
       .set("Authorization", `Bearer ${apiToken}`)
       .expect(200)
+    });
+    
+    describe('Negative Tests', () => {
+      it("401 authentication failed", async () => {
+        const data = {
+          user_id: userId,
+          title: "My title",
+          body: "my blog post",
+        };
+        
+        const res = await request
+        .post("posts")
+        .send(data);
+        
+        expect(res.statusCode).to.eq(401);
+        expect(res.body.message).to.eq("Authentication failed");
+      });
+
+      it.only("No title returns 422 validation failed", async () => {
+        const data = {
+          user_id: userId,
+          body: "my blog post",
+        };
+
+        const res = await request
+          .post("posts")
+          .set("Authorization", `Bearer ${apiToken}`)
+          .send(data);
+        
+        expect(res.statusCode).to.eq(422);
+        expect(res.body[0].field).to.eq("title");
+        expect(res.body[0].message).to.eq("can't be blank");
+      });
+    });
   });
-});
+  
