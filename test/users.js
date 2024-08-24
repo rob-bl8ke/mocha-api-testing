10c2eded15e5599f3fce841dba31df4465615ee4
Rob Blake <rob.bl8ke@outlook.com>
2024-08-24 11:37:11 +0200

    First successful fail/pass text (remember test is async)

--- test/users.js
+++ test/users.js
@@ -1,16 +1,17 @@
 import supertest from 'supertest';
 import "dotenv/config";
+import { expect } from 'chai';
 
 const apiToken = process.env.TOKEN;
 const request = supertest('https://gorest.co.in/public/v2/')
 
 describe('Users', () => {
-    it('GET /users', () => {
-        request.get(`users?access-token=${apiToken}`)
-            .end((err, res) => {
-                console.log(err);
-                console.log(res.body);
-            });
-        ;
+    it('GET /users', (done) => {
+      request.get(`users?access-token=${apiToken}`).end((err, res) => {
+        console.log(res.body);
+        // expect(res.body).to.be.empty;
+        expect(res.body).to.not.be.empty;
+        done();
+      });
     });
 });
