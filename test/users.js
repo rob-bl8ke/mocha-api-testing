597ab538cf0c99848a4f413b8627b0c4896078a9
Rob Blake <rob.bl8ke@outlook.com>
2024-08-24 16:01:17 +0200

    Reorganize to do CRUD (but these tests are dependent... you don't like this)

--- test/users.js
+++ test/users.js
@@ -6,75 +6,85 @@ const apiToken = process.env.TOKEN;
 const request = supertest('https://gorest.co.in/public/v2/')
 
 describe('Users', () => {
-    it('GET /users', () => {
-      return request.get(`users?access-token=${apiToken}`).then((res) => {
-        expect(res.body).to.not.be.empty; });
-    });
+    let userId;
 
-    it('GET /users/:id', () => {
-        return request
-        .get(`users/7357440?access-token=${apiToken}`)
-        .then((res) => {
-            expect(res.body).to.be.eq(7357440);
+    describe('POST', () => {
+        it('/users', () => {
+            const data = {
+                email: `bluesmurf${Math.floor(Math.random() * 9999)}@smurfmail.za`,
+                name: 'Test name',
+                gender: 'male',
+                status: 'inactive'
+            };
+    
+            return request.post('users')
+                .set('Authorization', `Bearer ${apiToken}`)
+                .send(data).then(res => {
+                    expect(res.body).to.deep.include(data);
+                    // set userId
+                    userId = res.body.id;
+            });
         });
+
     });
 
-    it('GET /users with query params', () => {
-        const url = `users?access-token=${apiToken}&page=2&gender=female&status=active`
+    describe('GET', () => {
+        it('/users', () => {
+          return request.get(`users?access-token=${apiToken}`).then((res) => {
+            expect(res.body).to.not.be.empty; });
+        });
+    
+        it('/users/:id', () => {
+            return request
+            .get(`users/${userId}?access-token=${apiToken}`)
+            .then((res) => {
+                expect(res.body.id).to.be.eq(userId);
+            });
+        });
 
-        return request.get(url).then(res => {
-            res.body.forEach(data => {
-                expect(data.gender).to.eq('female');
-                expect(data.status).to.eq('active');
+        it('/users with query params', () => {
+            const url = `users?access-token=${apiToken}&page=2&gender=female&status=active`
+    
+            return request.get(url).then(res => {
+                res.body.forEach(data => {
+                    expect(data.gender).to.eq('female');
+                    expect(data.status).to.eq('active');
+                });
             });
         });
     });
-
-    it('POST /users', () => {
-        const data = {
-            email: `bluesmurf${Math.floor(Math.random() * 9999)}@smurfmail.za`,
-            name: 'Test name',
-            gender: 'male',
-            status: 'inactive'
-        };
-
-        return request.post('users')
-            .set('Authorization', `Bearer ${apiToken}`)
-            .send(data).then(res => {
+    
+    describe('PUT', () => {
+        it('/users', () => {
+            const data = {
+                email: `fluffywhite${Math.floor(Math.random() * 9999)}@powerdogs.com`,
+                name: 'Fluffy White',
+                gender: 'male',
+                status: 'active'
+            };
+    
+            return request
+              .put(`users/${userId}`)
+              .set("Authorization", `Bearer ${apiToken}`)
+              .send(data)
+              .then((res) => {
                 // Assert everything...
                 // data.email = 'test@mail.co.za';
                 expect(res.body).to.deep.include(data);
-
+              });
         });
     });
 
-    it('PUT /users', () => {
-        const data = {
-            email: `fluffywhite${Math.floor(Math.random() * 9999)}@powerdogs.com`,
-            name: 'Fluffy White',
-            gender: 'male',
-            status: 'active'
-        };
-
-        return request
-          .put("users/7358006")
-          .set("Authorization", `Bearer ${apiToken}`)
-          .send(data)
-          .then((res) => {
-            // Assert everything...
-            // data.email = 'test@mail.co.za';
-            expect(res.body).to.deep.include(data);
-          });
-    });
-
-    it.only('PUT /users', () => {
-        const expectedResponse = {};
-
-        return request
-          .delete("users/7358002")
-          .set("Authorization", `Bearer ${apiToken}`)
-          .then((res) => {
-            expect(res.body).to.deep.include(expectedResponse);
-          });
+    describe('DELETE', () => {
+        it('/users', () => {
+            const expectedResponse = {};
+            
+            return request
+            .delete(`users/${userId}`)
+            .set("Authorization", `Bearer ${apiToken}`)
+            .then((res) => {
+                expect(res.body).to.deep.include(expectedResponse);
+            });
+        });
     });
 });
